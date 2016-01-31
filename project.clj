d3d161467cd37df00b7c67e43291931aaa21618c
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-01-31 21:06:36 +0100

    updated dependencies

--- project.clj
+++ project.clj
@@ -5,24 +5,24 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :pallet {:source-paths ["src"]}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.palletops/pallet "0.8.0-RC.11"]
-                 [com.palletops/pallet "0.8.0-RC.11" :classifier "tests"]
+                 [com.palletops/pallet "0.8.10"]
+                 [com.palletops/pallet "0.8.10" :classifier "tests"]
                  [com.palletops/stevedore "0.8.0-beta.7"]
                  [ch.qos.logback/logback-classic "1.0.9"]
-                 [org.domaindrivenarchitecture.org/dda-config-crate "0.2.0"]
-                 [org.domaindrivenarchitecture/dda-basic-crate "0.1.2"]
-                 [org.domaindrivenarchitecture/dda-collected-crate "0.1.0"]
-                 [org.domaindrivenarchitecture/dda-backup-crate "0.2.1"]
-                 [org.domaindrivenarchitecture/dda-tomcat-crate "0.1.0"]
-                 [org.domaindrivenarchitecture/dda-mysql-crate "0.1.0"]
-                 [org.domaindrivenarchitecture/httpd "0.2.0"]]
+                 [org.domaindrivenarchitecture.org/dda-config-crate "0.3.0-SNAPSHOT"]
+                 [org.domaindrivenarchitecture/dda-basic-crate "0.3.0-SNAPSHOT"]
+                 [org.domaindrivenarchitecture/dda-collected-crate "0.3.0-SNAPSHOT"]
+                 [org.domaindrivenarchitecture/dda-backup-crate "0.2.2-SNAPSHOT"]
+                 [org.domaindrivenarchitecture/dda-tomcat-crate "0.1.1-SNAPSHOT"]
+                 [org.domaindrivenarchitecture/dda-mysql-crate "0.1.1-SNAPSHOT"]
+                 [org.domaindrivenarchitecture/httpd "0.2.1-SNAPSHOT"]]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
   :deploy-repositories [["snapshots" :clojars]
                         ["releases" :clojars]]
   :profiles {:dev
              {:dependencies
-              [[com.palletops/pallet "0.8.0-RC.11" :classifier "tests"]
+              [[com.palletops/pallet "0.8.10" :classifier "tests"]
                ]
               :plugins
               [[com.palletops/pallet-lein "0.8.0-alpha.1"]]}
