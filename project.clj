c8c85c14c1c51e59814930cf233b77caf07b337d
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-04-28 11:14:32 +0200

    lein-release plugin: bumped version from 0.2.1 to 0.2.2-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/dda-liferay-crate "0.2.1"
+(defproject org.domaindrivenarchitecture/dda-liferay-crate "0.2.2-SNAPSHOT"
   :description "dda-liferay-crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
