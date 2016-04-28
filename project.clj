0634c96214c0ae04c7878530d3d1f40b87d154eb
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-04-28 11:14:00 +0200

    lein-release plugin: preparing 0.2.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/dda-liferay-crate "0.2.1-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/dda-liferay-crate "0.2.1"
   :description "dda-liferay-crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
@@ -7,17 +7,17 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [prismatic/schema "1.1.0"]
                  [metosin/schema-tools "0.9.0"]
-                 [com.palletops/pallet "0.8.11"]
+                 [com.palletops/pallet "0.8.12"]
                  [com.palletops/stevedore "0.8.0-beta.7"]
                  [com.palletops/git-crate "0.8.0-alpha.2" :exclusions [org.clojure/clojure]]
-                 [org.domaindrivenarchitecture/dda-config-commons "0.1.1-SNAPSHOT"]
-                 [org.domaindrivenarchitecture/dda-config-crate "0.3.2-SNAPSHOT"]
-                 [org.domaindrivenarchitecture/dda-basic-crate "0.3.2-SNAPSHOT"]
-                 [org.domaindrivenarchitecture/dda-collected-crate "0.3.2-SNAPSHOT"]
-                 [org.domaindrivenarchitecture/dda-backup-crate "0.3.2-SNAPSHOT"]
-                 [org.domaindrivenarchitecture/dda-tomcat-crate "0.1.3-SNAPSHOT"]
-                 [org.domaindrivenarchitecture/dda-mysql-crate "0.1.2-SNAPSHOT"]
-                 [org.domaindrivenarchitecture/httpd "0.2.1"]]
+                 [org.domaindrivenarchitecture/dda-config-commons "0.1.1"]
+                 [org.domaindrivenarchitecture/dda-config-crate "0.3.2"]
+                 [org.domaindrivenarchitecture/dda-basic-crate "0.3.2"]
+                 [org.domaindrivenarchitecture/dda-collected-crate "0.3.2"]
+                 [org.domaindrivenarchitecture/dda-backup-crate "0.3.2"]
+                 [org.domaindrivenarchitecture/dda-tomcat-crate "0.1.3"]
+                 [org.domaindrivenarchitecture/dda-mysql-crate "0.1.2"]
+                 [org.domaindrivenarchitecture/httpd "0.2.2"]]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
   :deploy-repositories [["snapshots" :clojars]
@@ -25,8 +25,8 @@
   :profiles {:dev
              {:dependencies
               [[org.clojure/test.check "0.9.0"]
-               [com.palletops/pallet "0.8.11" :classifier "tests"]
-               [org.domaindrivenarchitecture/dda-pallet-commons "0.1.1" :classifier "tests"]]
+               [com.palletops/pallet "0.8.12" :classifier "tests"]
+               [org.domaindrivenarchitecture/dda-pallet-commons "0.1.2" :classifier "tests"]]
               :plugins
               [[com.palletops/pallet-lein "0.8.0-alpha.1"]]}
               :leiningen/reply
