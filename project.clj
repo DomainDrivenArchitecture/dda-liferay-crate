a52f3bab721663a304010574cfba7a0bed051e2a
Clemens Geibel <clemens.geibel@meissa-gmbh.de>
2016-11-14 12:17:08 +0100

    lein-release plugin: bumped version from 0.2.5 to 0.2.6-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/dda-liferay-crate "0.2.5"
+(defproject org.domaindrivenarchitecture/dda-liferay-crate "0.2.6-SNAPSHOT"
   :description "dda-liferay-crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
