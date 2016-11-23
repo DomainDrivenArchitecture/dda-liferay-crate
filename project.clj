5b0b25627067033e292f3117fdc41744457f2afc
Jan Krebs <jan.krebs@meissa-gmbh.de>
2016-11-23 15:27:15 +0100

    lein-release plugin: preparing 0.2.6 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/dda-liferay-crate "0.2.6-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/dda-liferay-crate "0.2.6"
   :description "dda-liferay-crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
@@ -8,13 +8,13 @@
                  [prismatic/schema "1.1.3"]
                  [com.palletops/pallet "0.8.12"]
                  [com.palletops/stevedore "0.8.0-beta.7"]
-                 [org.domaindrivenarchitecture/dda-config-commons "0.1.6"]
-                 [org.domaindrivenarchitecture/dda-pallet-commons "0.2.0"]
-                 [org.domaindrivenarchitecture/dda-pallet "0.2.0-SNAPSHOT"]
-                 [org.domaindrivenarchitecture/dda-backup-crate "0.4.0-SNAPSHOT"]
-                 [org.domaindrivenarchitecture/dda-tomcat-crate "0.1.4"]
-                 [org.domaindrivenarchitecture/dda-mysql-crate "0.1.3"]                 
-                 [org.domaindrivenarchitecture/dda-httpd-crate "0.1.2"]]
+                 [org.domaindrivenarchitecture/dda-config-commons "0.1.7"]
+                 [org.domaindrivenarchitecture/dda-pallet-commons "0.2.1"]
+                 [org.domaindrivenarchitecture/dda-pallet "0.2.0"]
+                 [org.domaindrivenarchitecture/dda-backup-crate "0.4.0"]
+                 [org.domaindrivenarchitecture/dda-tomcat-crate "0.1.5"]
+                 [org.domaindrivenarchitecture/dda-mysql-crate "0.1.4"]                 
+                 [org.domaindrivenarchitecture/dda-httpd-crate "0.1.3"]]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
   :deploy-repositories [["snapshots" :clojars]
@@ -25,7 +25,7 @@
               [[org.clojure/test.check "0.9.0"]               
                [org.clojure/tools.cli "0.3.5"]
                [com.palletops/stevedore "0.8.0-beta.7"]
-               [org.domaindrivenarchitecture/dda-pallet-commons "0.2.0" :classifier "tests"]
+               [org.domaindrivenarchitecture/dda-pallet-commons "0.2.1" :classifier "tests"]
                [com.palletops/pallet "0.8.12" :classifier "tests"]
                [org.domaindrivenarchitecture/pallet-aws "0.2.8-SNAPSHOT"]
                [org.slf4j/jcl-over-slf4j "1.7.21"]]
