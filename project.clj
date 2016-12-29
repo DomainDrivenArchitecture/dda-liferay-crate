9118b21fc0b83887c535653a7ee2c67c8acc4e7b
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-12-29 15:59:12 +0100

    updated deps

--- project.clj
+++ project.clj
@@ -9,12 +9,12 @@
                  [com.palletops/pallet "0.8.12"]
                  [com.palletops/stevedore "0.8.0-beta.7"]
                  [org.domaindrivenarchitecture/dda-config-commons "0.1.7"]
-                 [org.domaindrivenarchitecture/dda-pallet-commons "0.2.1"]
-                 [org.domaindrivenarchitecture/dda-pallet "0.2.1-SNAPSHOT"]
+                 [org.domaindrivenarchitecture/dda-pallet-commons "0.3.0"]
+                 [org.domaindrivenarchitecture/dda-pallet "0.3.0"]
                  [org.domaindrivenarchitecture/dda-backup-crate "0.4.1-SNAPSHOT"]
                  [org.domaindrivenarchitecture/dda-tomcat-crate "0.1.5"]
                  [org.domaindrivenarchitecture/dda-mysql-crate "0.1.4"]                 
-                 [org.domaindrivenarchitecture/dda-httpd-crate "0.1.3"]]
+                 [org.domaindrivenarchitecture/dda-httpd-crate "0.1.4-SNAPSHOT"]]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
   :deploy-repositories [["snapshots" :clojars]
@@ -25,14 +25,14 @@
               [[org.clojure/test.check "0.9.0"]               
                [org.clojure/tools.cli "0.3.5"]
                [com.palletops/stevedore "0.8.0-beta.7"]
-               [org.domaindrivenarchitecture/dda-pallet-commons "0.2.1" :classifier "tests"]
+               [org.domaindrivenarchitecture/dda-pallet-commons "0.3.0" :classifier "tests"]
                [com.palletops/pallet "0.8.12" :classifier "tests"]
                [org.domaindrivenarchitecture/pallet-aws "0.2.8-SNAPSHOT"]
-               [org.slf4j/jcl-over-slf4j "1.7.21"]]
+               [org.slf4j/jcl-over-slf4j "1.7.22"]]
               :plugins
               [[lein-sub "0.3.0"]]}
               :leiningen/reply
-               {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.21"]]
+               {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.22"]]
                 :exclusions [commons-logging]}}
   :local-repo-classpath true
   :classifiers {:tests {:source-paths ^:replace ["test" "integration"]
