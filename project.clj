090b8f6f4db44341316b4d74752af49122da5097
Lucca Hellriegel <lucca.hellriegel@meissa-gmbh.de>
2018-06-25 10:02:12 +0200

    clojure 1.9

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [dda/dda-pallet "1.0.1"]
-                 [dda/dda-httpd-crate "1.0.0"]
-                 [dda/dda-mariadb-crate "1.0.0"]
-                 [dda/dda-tomcat-crate "1.0.0"]
-                 [dda/dda-backup-crate "0.7.1"]]
+  :dependencies [[dda/dda-pallet "2.2.0"]
+                 [dda/dda-httpd-crate "2.0.7"]
+                 [dda/dda-mariadb-crate "1.0.3"]
+                 [dda/dda-tomcat-crate "2.0.1-SNAPSHOT"]
+                 [dda/dda-backup-crate "1.0.4"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
@@ -22,22 +21,32 @@
                                     "test/resources"]
                    :dependencies
                    [[org.clojure/test.check "0.10.0-alpha2"]
-                    [org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
-                    [com.palletops/pallet "0.8.12" :classifier "tests"]
-                    [ch.qos.logback/logback-classic "1.2.3"]
-                    [org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
+                    [org.domaindrivenarchitecture/pallet-aws "0.2.8.2"
+                      :exclusions [com.palletops/pallet]]
+                    [dda/pallet "0.9.0" :classifier "tests"]
+                    [ch.qos.logback/logback-classic "1.3.0-alpha4"]
+                    [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]
                    :plugins
                    [[lein-sub "0.3.0"]]
-                   :repl-options {:init-ns dda.pallet.dda-liferay-crate.app.instantiate-aws}
+                   :repl-options {:init-ns dda.pallet.dda-liferay-crate.app.instantiate-existing}
                    :leiningen/reply
                     {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
                      :exclusions [commons-logging]}}
              :test {:test-paths ["test/src"]
                     :resource-paths ["test/resources"]
-                    :dependencies [[com.palletops/pallet "0.8.12" :classifier "tests"]]}
+                    :dependencies [[dda/pallet "0.9.0" :classifier "tests"]]}
              :uberjar {:source-paths ["uberjar/src"]
                        :resource-paths ["uberjar/resources"]
                        :aot :all
                        :main dda.pallet.dda-liferay-crate.main
-                       :dependencies [[org.clojure/tools.cli "0.3.5"]]}}
+                       :dependencies [[org.clojure/tools.cli "0.3.7"]]}}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy"]
+                  ["uberjar"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
   :local-repo-classpath true)
